edfa1620a40c42173eded2096f7643e9a11cd34f
ik012982i9 <a@a.a>
2024-05-13 12:51:28 +0200

    Añadir contenido principal

---
+++ script.js
@@ -0,0 +1,28 @@
+const targetDate = new Date('2024-06-24:09:50');
+
+function updateCountdown() {
+  const now = new Date();
+  const difference = targetDate - now;
+
+  if (difference <= 0) {
+    document.getElementById('countdown').style.display = 'none';
+    document.getElementById('myButton').style.display = 'block';
+    document.getElementById('myButton').addEventListener('click', redirectToOtherPage);
+  } else {
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    const countdown = `${days} días, ${hours} horas, ${minutes} minutos, ${seconds} segundos`;
+    document.getElementById('countdown').innerText = countdown;
+  }
+}
+
+function redirectToOtherPage() {
+  window.location.href = "otra_pagina.html";
+}
+
+setInterval(updateCountdown, 1000);
+
+updateCountdown();
